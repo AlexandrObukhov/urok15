6ccf205e6404698d07ac7c8209a73a72d8eda8d7
AlexandrObukhov <shurpovich@gmail.com>
2021-07-04 14:22:00 +0300

    a

--- project/js/scripts.js
+++ project/js/scripts.js
@@ -16,17 +16,91 @@ $(function () {
 
 	$('button.basket').click(function () {
 		$('.popup-desk').addClass('active');
+		$('.popup').html('<p class="popup-header">Личный кабинет</p><input type="text" name="fullname" placeholder="Логин"><input type="password" name="password" placeholder="Пароль"><button type="submit">Войти</button><a href="https://yandex.ru/" class="register">Зарегистрироваться</a>');
+		$('.basket').html(6);
 	});
 
 	$('.popup-desk').click(function (e) {
 		if (e.target == this) {
 			$(this).removeClass('active');
+			$('.popup').empty();
+			$('.basket').html(5);
 		}
 	});
 
-	$('.register').click(function (e) {
+	$(document).on('click', '.register', function (e) {
 		e.preventDefault();
+		if ($('.basket').html() == 6) {
+			$('.popup').html('<p class="popup-header">Личный кабинет закрыт на ремонт.<br>Регистрации не будет до 1 января.</p><a href="https://yandex.ru/" class="register">Войти</a>');
+			$('.basket').html(5);
+		} else {
+			$('.popup').html('<p class="popup-header">Личный кабинет</p><input type="text" name="fullname" placeholder="Логин"><input type="password" name="password" placeholder="Пароль"><button type="submit">Войти</button><a href="https://yandex.ru/" class="register">Зарегистрироваться</a>');
+			$('.basket').html(6);
+		}
+	});
 
-		$('.popup').html('<p class="popup-header">Личный кабинет закрыт на ремонт.<br>Регистрации не будет до 1 января.</p>')
+	$(document).on('click', 'button[type="submit"]', function () {
+		alert('OGOGO!!!');
+	});
+
+	$(document).on('click', '.order .del > div', function () {
+		tovarDelete(this);
+	});
+
+	$(document).on('change input', '.order .num > input', function () {
+		tovarChange(this);
 	});
 });
+
+
+
+/* order */
+const order = [
+	{
+		id: 5711,
+		name: 'Товар 1',
+		value: 10,
+		price: 5
+	},
+	{
+		id: 3432,
+		name: 'Товар 2',
+		value: 10,
+		price: 10
+	},
+	{
+		id: 4846,
+		name: 'Товар 3',
+		value: 10,
+		price: 15
+	}
+]
+function tovarDelete(point) {
+	let b = point.parentNode.parentNode;
+	let t_id = b.querySelector('th').dataset.tovar;
+	for (let i = 0; i < order.length; i++) {
+		if (order[i].id == t_id) {
+			order.splice(i, 1);
+			console.log(t_id); // имитация отправки бэку сообщения об удалении товара
+			break;
+		}
+	}
+	b.remove();
+	if (order.length > 0) {
+		tovarCount();
+	} else {
+		document.querySelector('.table').remove();
+		document.querySelector('.nihil').style = 'display:block';
+	}
+}
+function tovarChange() {
+	// опознать товар
+	// найти его в ордере
+	// изменить количество в ордере
+	// вызвать пересчет +
+
+	tovarCount();
+}
+function tovarCount() {
+	alert('It works!');
+}
